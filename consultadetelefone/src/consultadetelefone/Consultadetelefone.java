adea439a39011d2d23987b72ce0e4546a6fac858
Maique07 <152615451+Maique07@users.noreply.github.com>
2024-08-23 20:29:16 -0300

    Add files via upload

--- consultadetelefone/src/consultadetelefone/Consultadetelefone.java
+++ consultadetelefone/src/consultadetelefone/Consultadetelefone.java
@@ -12,7 +12,8 @@ public class Consultadetelefone {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int x,num_de_consult;
+        int x, num_de_consult;
+        String a;
         String s = JOptionPane.showInputDialog("Informe a quantidade de telefone a serem cadastrados.");
         x = Integer.parseInt(s);
         while (x <= 0) {
@@ -21,21 +22,23 @@ public class Consultadetelefone {
         }
         String nome[] = new String[x];
         String tel[] = new String[x];
-        for(int i = 0; i < x; i++){
-         nome[i] = JOptionPane.showInputDialog("Informe o nome da pessoa "+(i+1)+":");
-         tel[i] = JOptionPane.showInputDialog("Informe o número da pessoa "+(i+1)+":");
-        }     
-        JOptionPane.showMessageDialog(null,"cadastramento feito com sucesso!");
-        String a = JOptionPane.showInputDialog("Informe o nome da pessoa para encontra o telefone desejado:");
-        for(int y = 0; y < x;y++){
-            if(a.equalsIgnoreCase(nome[y])){
-                JOptionPane.showMessageDialog(null,"Nome encontrado\nNome: "+ nome[y]+"\nTelefone: "+ tel[y]);
+        for (int i = 0; i < x; i++) {
+            nome[i] = JOptionPane.showInputDialog("Informe o nome da pessoa " + (i + 1) + ":");
+            tel[i] = JOptionPane.showInputDialog("Informe o número da pessoa " + (i + 1) + ":");
+        }
+        JOptionPane.showMessageDialog(null, "cadastramento feito com sucesso!");
+        a = JOptionPane.showInputDialog("Informe o nome da pessoa para encontra o telefone desejado:");
+        int y;
+        for (y = 0; y < x; y++) {
+            if (a.equals(nome[y])) {
+                JOptionPane.showMessageDialog(null, "Numero encontrado\nNome: " + nome[y] + "\nTelefone: " + tel[y]);
                 break;
-            }else{
-                JOptionPane.showMessageDialog(null, "Nome inexistente no banco de dados.");
-                break; 
             }
         }
+        if (y == x) {
+            JOptionPane.showMessageDialog(null, "Nome inexistente no banco de dados.");
+        }
+
     }
-    
+
 }
